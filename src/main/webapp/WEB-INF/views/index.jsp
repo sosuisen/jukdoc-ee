9d2fb29aeb2ed796bfc8b85df2719ae364c50f35
sosuisen <hidekazu.kubota@gmail.com>
2024-11-01 22:56:36 +0900

    format: .jsp, .css. js

--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,37 +1,37 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="jakarta.tags.core"%>
+         pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<link href="${mvc.basePath}/../app.css" rel="stylesheet">
-<title>Jukdoc</title>
+    <meta charset="UTF-8">
+    <link href="${mvc.basePath}/../app.css" rel="stylesheet">
+    <title>Jukdoc</title>
 </head>
 <body
-	x-data="{ paragraphs: [] }"
-	x-init="$get('/paragraphs', { error: 'Cannot get paragraphs' }).then(res => { if (res.status==200) paragraphs = res.data })">
+        x-data="{ paragraphs: [] }"
+        x-init="$get('/paragraphs', { error: 'Cannot get paragraphs' }).then(res => { if (res.status==200) paragraphs = res.data })">
 
-	<h1>Original Document</h1>
+<h1>Original Document</h1>
 
-	<div style="color: red" x-show="$store.errors.length > 0">
-		<template x-for="error in $store.errors">
-			<div x-text="error"></div>
-		</template>
-	</div>
+<div style="color: red" x-show="$store.errors.length > 0">
+    <template x-for="error in $store.errors">
+        <div x-text="error"></div>
+    </template>
+</div>
 
-	<div x-data="{ markAsRead(tag) { (paragraphs.find(para => para.positionTag === tag)).read = true; } }">
-	    <template x-for="para in paragraphs">
-	        <div :class="(para.read ? 'read' : 'unread') + ' ' + (para.header ? 'header' : 'paragraph')">
-	        	<button @click="markAsRead(para.positionTag)">MarkAsRead</button>
-			    <span x-text="para.paragraph"></span>(<span x-text="para.positionTag"></span>)
-		    </div>
-	    </template>
-	</div>
+<div x-data="{ markAsRead(tag) { (paragraphs.find(para => para.positionTag === tag)).read = true; } }">
+    <template x-for="para in paragraphs">
+        <div :class="(para.read ? 'read' : 'unread') + ' ' + (para.header ? 'header' : 'paragraph')">
+            <button @click="markAsRead(para.positionTag)">MarkAsRead</button>
+            <span x-text="para.paragraph"></span>(<span x-text="para.positionTag"></span>)
+        </div>
+    </template>
+</div>
 
-	<script type="module">
-		import rest from '${mvc.basePath}/../rest.js';
-		rest.start('${mvc.basePath}/api', '${mvc.csrf.token}');
-	</script>
+<script type="module">
+    import rest from '${mvc.basePath}/../rest.js';
+
+    rest.start('${mvc.basePath}/api', '${mvc.csrf.token}');
+</script>
 </body>
 </html>
--- src/main/webapp/rest.js
+++ src/main/webapp/rest.js
@@ -14,85 +14,85 @@ import Alpine from './webjars/alpinejs/3.14.3/dist/module.esm.js';
  * @param {string} csrfToken - CSRF token
  */
 const start = (rootEndpointURL, csrfToken) => {
-	delete rest.start;
+    delete rest.start;
 
-	// Alpine.js store. Accessible as $successes and $errors from the Alpine components in the HTML.
-	Alpine.store('successes', []);
-	Alpine.store('errors', []);
+    // Alpine.js store. Accessible as $successes and $errors from the Alpine components in the HTML.
+    Alpine.store('successes', []);
+    Alpine.store('errors', []);
 
-	const client = axios.create({
-		baseURL: rootEndpointURL,
-		headers: {
-			'Content-Type': 'application/json',
-			'X-CSRF-TOKEN': csrfToken
-		},
-	});
+    const client = axios.create({
+        baseURL: rootEndpointURL,
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken
+        },
+    });
 
-	const showError = (e, defaultErrorMessage = 'Error') => {
-	    // The response body can be obtained from e.response.data in the Axios error object.
+    const showError = (e, defaultErrorMessage = 'Error') => {
+        // The response body can be obtained from e.response.data in the Axios error object.
         // Here, we assume that the server returns the following body for a 400 error:
         // {type: 'constraint_error', errors: ['Error 1', 'Error 2', ...] }
-		if (e.response?.data?.type == 'constraint_error' && e.response?.data?.errors) {
-			Alpine.store('errors').push(...e.response.data.errors);
-		}
-		// Other error responses
-		else if (e.response) {
-			Alpine.store('errors').push(defaultErrorMessage + ': ' + e.response.status + " " + e.response.statusText);
-		}
-		// If e.response is not present, the error occurred on the client side
-		else {
-			Alpine.store('errors').push(defaultErrorMessage + ': ' + e.message);
-		}
-	};
+        if (e.response?.data?.type == 'constraint_error' && e.response?.data?.errors) {
+            Alpine.store('errors').push(...e.response.data.errors);
+        }
+        // Other error responses
+        else if (e.response) {
+            Alpine.store('errors').push(defaultErrorMessage + ': ' + e.response.status + " " + e.response.statusText);
+        }
+        // If e.response is not present, the error occurred on the client side
+        else {
+            Alpine.store('errors').push(defaultErrorMessage + ': ' + e.message);
+        }
+    };
 
-	 /**
-      * Returns a function to call the REST API with the specified method.
-      * @param {string} method - The HTTP method to use ('get', 'post', 'put', 'delete', etc.).
-      */
-	const restFetch = (method) =>
-		 /**
-          * Common process for REST API calls
-          * Adds messages corresponding to the result in $successes and $errors, and returns the response.
-          *
-          * @param {string} endpoint - URL path after the root endpoint
-          * @param {Object} options - Request options.
-          * @param {Object} options.param - Object to send as JSON.
-          * @param {string} [options.success] - Message on success.
-          * @param {string} [options.error] - Default message on error.
-          * @returns {Promise<Object>} An object containing the response status and data.
-          * @returns {number} return.status - HTTP response status.
-          * @returns {Object|null} return.data - Response data or null (on error).
-          */
-		async (endpoint, { param, success, error }) => {
-			Alpine.store('successes').length = 0;
-			Alpine.store('errors').length = 0;
-			let status;
-			return client[method](endpoint, param)
-				.then(res => {
-					status = res.status;
-					return res.data;
-				})
-				.then(receivedJson => {
-					if (success) Alpine.store('successes').push(success)
-					console.debug(receivedJson);
-					return { status, data: receivedJson };
-				})
-				.catch(e => {
-					showError(e, error);
-					return { status: e.response.status, data: null };
-				});
-		};
-	Alpine.magic('get', () => restFetch('get'));
-	Alpine.magic('post', () => restFetch('post'));
-	Alpine.magic('put', () => restFetch('put'));
-	Alpine.magic('delete', () => restFetch('delete'));
-	Alpine.magic('rest', () => method => restFetch(method));
+    /**
+     * Returns a function to call the REST API with the specified method.
+     * @param {string} method - The HTTP method to use ('get', 'post', 'put', 'delete', etc.).
+     */
+    const restFetch = (method) =>
+        /**
+         * Common process for REST API calls
+         * Adds messages corresponding to the result in $successes and $errors, and returns the response.
+         *
+         * @param {string} endpoint - URL path after the root endpoint
+         * @param {Object} options - Request options.
+         * @param {Object} options.param - Object to send as JSON.
+         * @param {string} [options.success] - Message on success.
+         * @param {string} [options.error] - Default message on error.
+         * @returns {Promise<Object>} An object containing the response status and data.
+         * @returns {number} return.status - HTTP response status.
+         * @returns {Object|null} return.data - Response data or null (on error).
+         */
+        async (endpoint, {param, success, error}) => {
+            Alpine.store('successes').length = 0;
+            Alpine.store('errors').length = 0;
+            let status;
+            return client[method](endpoint, param)
+                .then(res => {
+                    status = res.status;
+                    return res.data;
+                })
+                .then(receivedJson => {
+                    if (success) Alpine.store('successes').push(success)
+                    console.debug(receivedJson);
+                    return {status, data: receivedJson};
+                })
+                .catch(e => {
+                    showError(e, error);
+                    return {status: e.response.status, data: null};
+                });
+        };
+    Alpine.magic('get', () => restFetch('get'));
+    Alpine.magic('post', () => restFetch('post'));
+    Alpine.magic('put', () => restFetch('put'));
+    Alpine.magic('delete', () => restFetch('delete'));
+    Alpine.magic('rest', () => method => restFetch(method));
 
-	window.Alpine = Alpine;
-	Alpine.start();
+    window.Alpine = Alpine;
+    Alpine.start();
 };
 
 const rest = {
-	start,
+    start,
 };
 export default rest;
