f0dce55e618adfc0d79e54525a6f33300d364c11
sosuisen <hidekazu.kubota@gmail.com>
2024-11-02 17:24:48 +0900

    feat: add RAG scenerio

--- src/main/java/net/sosuisen/resources/Chat.java
+++ src/main/java/net/sosuisen/resources/Chat.java
@@ -1,12 +1,17 @@
 package net.sosuisen.resources;
 
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import net.sosuisen.model.*;
 import net.sosuisen.service.ParagraphService;
@@ -16,8 +21,11 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -34,6 +42,16 @@ public class Chat {
     private final ChatCommand chatCommand;
     private final ParagraphService paragraphService;
     private final QAService qaService;
+
+    OpenAiChatModel chatModel;
+
+    @PostConstruct
+    public void init() {
+        chatModel = OpenAiChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName(GPT_4_O_MINI)
+                .build();
+    }
 
     @GET
     @Path("opening-words")
@@ -59,11 +77,16 @@ public class Chat {
                 if (RE_ANAPHORA.matcher(mes).find()) {
                     System.out.println("includes anaphora");
                 } else {
-                    var retrievalDocs = retrieveContext(mes);
-                    var context = retrievalDocs.stream()
-                            .map(Document::toString)
-                            .collect(Collectors.joining("\n\n"));
-                    System.out.println("context: " + context);
+                    var retrievalDocs = retrieveDocuments(mes);
+                    if (!retrievalDocs.isEmpty()) {
+                        StreamingChatLanguageModel model = OpenAiStreamingChatModel.builder()
+                                .apiKey(System.getenv("OPENAI_API_KEY"))
+                                .modelName(GPT_4_O_MINI)
+                                .build();
+                        var response = proceedByPrompt("", retrievalDocs, mes);
+                        System.out.println("response: " + response);
+                        return new ChatMessage("AI", response);
+                    }
                 }
             }
 
@@ -72,7 +95,65 @@ public class Chat {
         return new ChatMessage("AI", "test");
     }
 
-    private ArrayList<Document> retrieveContext(String message) throws SQLException {
+    private String getPrompt(String log, ArrayList<Document> retrievalDocs, String query) {
+        var count = 0;
+        var contextBuilder = new StringBuilder();
+        var refsBuilder = new StringBuilder();
+        var refsHintBuilder = new StringBuilder();
+
+        for (var doc : retrievalDocs) {
+            count++;
+            contextBuilder.append("[reference: ").append(count).append("] ").append(doc.getContext()).append("\n");
+            refsBuilder.append("[reference: ").append(count).append("], ");
+            refsHintBuilder.append("* If the response includes the above 【reference: ").append(count).append("], ")
+                    .append("add [*").append(count).append("] at the end of the sentence.\n");
+        }
+
+        if (!refsBuilder.isEmpty()) {
+            refsBuilder.insert(0, "please refer to the following references:");
+        }
+        if (contextBuilder.isEmpty()) {
+            contextBuilder.append("(No relevant information found.)");
+        }
+
+        var prompt = """
+                In the past, we discussed the following topics:
+                %s
+                
+                Additionally, the following related conversations took place:
+                %s
+                
+                Building upon these discussions, %s generate the best possible answer to my question.
+                
+                However, please adhere to the following "Constraints"
+                
+                [Constraints]
+                * If a direct answer cannot be provided based on the above text, you will preface your response concisely with, "As the direct answer is not contained in this document, I cannot provide an exact response, but".
+                * Replace outdated expressions with modern ones.
+                * Avoid repeating the question in the response.
+                * Ensure the response is easy for elementary-level readers to understand.
+                * Do not omit any important keywords.
+                * Keep idioms intact.
+                * Begin with a concise summary, followed by a detailed explanation.
+                * Add a line break at the end of each sentence.
+                %s
+                
+                [My Question] %s
+                """.formatted(log, contextBuilder.toString(), refsBuilder.toString(), refsHintBuilder.toString(), query);
+
+        System.out.println("prompt: " + prompt);
+        return prompt;
+    }
+
+    private String proceedByPrompt(String log, ArrayList<Document> retrievalDocs, String query) {
+        var prompt = getPrompt(log, retrievalDocs, query);
+        var response = chatModel.generate(List.of(
+                SystemMessage.from("You are a skilled assistant."),
+                UserMessage.from(prompt)));
+        return response.content().text();
+    }
+
+    private ArrayList<Document> retrieveDocuments(String message) throws SQLException {
         var maxParagraphDocs = 1;
         var paragraphDocs = paragraphService.query(message, maxParagraphDocs, 0.8);
         var qaDocs = qaService.query(message, 3, 0.8);
@@ -85,6 +166,11 @@ public class Chat {
             mergedDocs.addAll(qaDocs);
         }
         mergedDocs.sort(Comparator.comparingDouble(Document::getScore).reversed());
+
+        var context = mergedDocs.stream()
+                .map(Document::toString)
+                .collect(Collectors.joining("\n\n"));
+        System.out.println("context: " + context);
 
         return mergedDocs;
     }
